e67086d33947bd3dd7d271bb658b9fc23d1f249e
Alexanik12 <afrintitu48@gmail.com>
2025-03-02 06:19:12 -0800

    added main.html

---
+++ script/none.js
@@ -0,0 +1,6 @@
+document.getElementById('task-1').style.display='none';
+document.getElementById('task-2').style.display='none';
+document.getElementById('task-3').style.display='none';
+document.getElementById('task-4').style.display='none';
+document.getElementById('task-5').style.display='none';
+document.getElementById('task-6').style.display='none';
--- script/script.js
+++ script/script.js
@@ -1,9 +1,128 @@
-document.getElementById('task-completed').style.display='none';
+const massage = 'board updated successfully';
 document.getElementById('completed').addEventListener('click', 
     function (event) {
         event.preventDefault();
         
-            document.getElementById('task-completed').style.display='block';
-            alert('first task completed ')
-    
+            document.getElementById('task-1').style.display='block';
+            alert(massage);
+            const click = document.getElementById('completed');
+            const point = document.getElementById('num').innerText;
+            const convertedPoint = parseInt(point);
+            const add = document.getElementById('add').innerText;
+            const convertedAdd = parseInt(add);
+            
+                const sum = convertedPoint - 1;
+                document.getElementById('num').innerText= sum;
+                const Sum = convertedAdd + 1;
+
+                document.getElementById('add').innerText= Sum;
+                click.disabled= true;
+            
+   
+})
+document.getElementById('completed-2').addEventListener('click', 
+    function (event) {
+        event.preventDefault();
+        
+            document.getElementById('task-2').style.display='block';
+            alert(massage);
+            const click = document.getElementById('completed-2');
+            const point = document.getElementById('num').innerText;
+            const convertedPoint = parseInt(point);
+            const add = document.getElementById('add').innerText;
+            const convertedAdd = parseInt(add);
+            
+                const sum = convertedPoint - 1;
+                document.getElementById('num').innerText= sum;
+                const Sum = convertedAdd + 1;
+
+                document.getElementById('add').innerText= Sum;
+                click.disabled= true;
+            
+   
+})
+document.getElementById('completed-3').addEventListener('click', 
+    function (event) {
+        event.preventDefault();
+        
+            document.getElementById('task-3').style.display='block';
+            alert(massage);
+            const click = document.getElementById('completed-3');
+            const point = document.getElementById('num').innerText;
+            const convertedPoint = parseInt(point);
+            const add = document.getElementById('add').innerText;
+            const convertedAdd = parseInt(add);
+            
+                const sum = convertedPoint - 1;
+                document.getElementById('num').innerText= sum;
+                const Sum = convertedAdd + 1;
+
+                document.getElementById('add').innerText= Sum;
+                click.disabled= true;
+            
+   
+})
+document.getElementById('completed-4').addEventListener('click', 
+    function (event) {
+        event.preventDefault();
+        
+            document.getElementById('task-4').style.display='block';
+            alert(massage);
+            const click = document.getElementById('completed-4');
+            const point = document.getElementById('num').innerText;
+            const convertedPoint = parseInt(point);
+            const add = document.getElementById('add').innerText;
+            const convertedAdd = parseInt(add);
+            
+                const sum = convertedPoint - 1;
+                document.getElementById('num').innerText= sum;
+                const Sum = convertedAdd + 1;
+
+                document.getElementById('add').innerText= Sum;
+                click.disabled= true;
+            
+   
+})
+document.getElementById('completed-5').addEventListener('click', 
+    function (event) {
+        event.preventDefault();
+        
+            document.getElementById('task-5').style.display='block';
+            alert(massage);
+            const click = document.getElementById('completed-5');
+            const point = document.getElementById('num').innerText;
+            const convertedPoint = parseInt(point);
+            const add = document.getElementById('add').innerText;
+            const convertedAdd = parseInt(add);
+            
+                const sum = convertedPoint - 1;
+                document.getElementById('num').innerText= sum;
+                const Sum = convertedAdd + 1;
+
+                document.getElementById('add').innerText= Sum;
+                click.disabled= true;
+            
+   
+})
+document.getElementById('completed-6').addEventListener('click', 
+    function (event) {
+        event.preventDefault();
+        
+            document.getElementById('task-6').style.display='block';
+            alert(massage);
+            alert('congrats!!! you have completed all the current task')
+            const click = document.getElementById('completed-6');
+            const point = document.getElementById('num').innerText;
+            const convertedPoint = parseInt(point);
+            const add = document.getElementById('add').innerText;
+            const convertedAdd = parseInt(add);
+            
+                const sum = convertedPoint - 1;
+                document.getElementById('num').innerText= sum;
+                const Sum = convertedAdd + 1;
+
+                document.getElementById('add').innerText= Sum;
+                click.disabled= true;
+            
+   
 })
